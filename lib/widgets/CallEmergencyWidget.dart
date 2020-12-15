fc554adb8ce8fd4550d566ffe600fda3f407d384
Satit Rianpit <siteslave@msn.com>
2020-12-15 09:46:07 +0700

    add tel

--- lib/widgets/CallEmergencyWidget.dart
+++ lib/widgets/CallEmergencyWidget.dart
@@ -1,6 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class CallEmergencyWidget extends StatelessWidget {
+
+  _launchURL() async {
+    const url = 'tel:1422';
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -16,31 +27,34 @@ class CallEmergencyWidget extends StatelessWidget {
                 fontWeight: FontWeight.w500),
           ),
         ),
-        Container(
-          margin: EdgeInsets.all(10),
-          padding: EdgeInsets.all(10),
-          // height: 100,
-          decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(15),
-              border: Border.all(color: Colors.amber)),
-          child: Row(
-            children: [
-              CircleAvatar(
-                child: Icon(
-                  Icons.call,
-                  color: Colors.amber[800],
+        GestureDetector(
+          onTap: () => _launchURL(),
+          child: Container(
+            margin: EdgeInsets.all(10),
+            padding: EdgeInsets.all(10),
+            // height: 100,
+            decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(15),
+                border: Border.all(color: Colors.amber)),
+            child: Row(
+              children: [
+                CircleAvatar(
+                  child: Icon(
+                    Icons.call,
+                    color: Colors.amber[800],
+                  ),
+                  backgroundColor: Colors.amber[50],
+                  radius: 30,
                 ),
-                backgroundColor: Colors.amber[50],
-                radius: 30,
-              ),
-              SizedBox(width: 18,),
-              Expanded(
-                  child: Text(
-                'โทร 1422 เพื่อขอคำปรึกษา หรือนัดเข้าตรวจหาเชื้อโควิด-19',
-                style: TextStyle(fontSize: 16),
-              ))
-            ],
+                SizedBox(width: 18,),
+                Expanded(
+                    child: Text(
+                  'โทร 1422 เพื่อขอคำปรึกษา หรือนัดเข้าตรวจหาเชื้อโควิด-19',
+                  style: TextStyle(fontSize: 16),
+                ))
+              ],
+            ),
           ),
         ),
       ],
